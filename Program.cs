fb17a5c0ecd62ccaf14a07572bd2aa43e0abdcdc
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-14 13:52:55 -0600

    will take a message with punctuation and upper case

--- Program.cs
+++ Program.cs
@@ -8,20 +8,27 @@ namespace CaesarCipher
         {
             char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
             Console.WriteLine("Enter the secret message");
-            string secretMessage = Console.ReadLine();
+            string secretMessage = Console.ReadLine().ToLower();
 
             char[] secretMessageArray = secretMessage.ToCharArray();
 
             char[] encryptedMessage = new char[secretMessageArray.Length];
 
+
             for (int i = 0; i < secretMessageArray.Length; i++)
-            {
-                char letter =  secretMessageArray[i];
+            {   
+                if (Char.IsPunctuation(secretMessageArray[i]))
+                {
+                    continue;
+                } else
+                {
+                    char letter =  secretMessageArray[i];
 
-                int alphabetPosition = Array.IndexOf(alphabet, letter);
-                int alphabetPositionPlus3 = (alphabetPosition + 3) % alphabet.Length;
+                    int alphabetPosition = Array.IndexOf(alphabet, letter);
+                    int alphabetPositionPlus3 = (alphabetPosition + 3) % alphabet.Length;
 
-                encryptedMessage[i] = alphabet[alphabetPositionPlus3];
+                    encryptedMessage[i] = alphabet[alphabetPositionPlus3];
+                }
 
             }
 
