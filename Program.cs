a29c3514178326848adba82264aba5996984c78f
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-14 12:39:55 -0600

    Update Program.cs
    Add string of chars and the ability to enter a secret message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+            Console.WriteLine("Enter the secret message");
+            string secretMessage = Console.ReadLine();
+
+            Console.WriteLine(secretMessage);
         }
     }
 }
